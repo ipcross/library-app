d8f7d21363b6d8a5aee67f3dae5496b4b595877e
cross <cross@vgg.ru>
2016-09-26 13:55:45 +0300

    Add ember-faker.

--- app/models/library.js
+++ app/models/library.js
@@ -6,7 +6,20 @@ export default DS.Model.extend({
   address: DS.attr('string'),
   phone: DS.attr('string'),
 
-  books: DS.hasMany('book'),
+  books: DS.hasMany('book', {inverse: 'library', async: true}),
 
-  isValid: Ember.computed.notEmpty('name')
+  isValid: Ember.computed.notEmpty('name'),
+
+  randomize() {
+    this.set('name', Faker.company.companyName() + ' Library');
+    this.set('address', this._fullAddress());
+    this.set('phone', Faker.phone.phoneNumber());
+
+    // If you would like to use in chain.
+    return this;
+  },
+
+  _fullAddress() {
+    return `${Faker.address.streetAddress()}, ${Faker.address.city()}`;
+  }
 });
